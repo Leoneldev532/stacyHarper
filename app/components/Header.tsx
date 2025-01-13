6abfa217625520345b8e0fd79d2adfebb21f05f6
leonel nextstep <leonelyimga@gmail.com>
2025-01-13 16:59:20 +0100

    Update ~ schema error

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -117,19 +117,6 @@ const tl = gsap.timeline({delay:3});
 
 
 
-const lenis = new Lenis()
-
-lenis.on('scroll', (e:any) => {
-  console.log(e)
-})
-
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
 
 
   return (
