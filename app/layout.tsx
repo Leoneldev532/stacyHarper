facf6f5927da1e6a4ac35ab8d729618683ce3079
leonel nextstep <leonelyimga@gmail.com>
2025-01-16 10:22:36 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "Ui Game build by Leonel Yimga ",
@@ -28,6 +29,12 @@ export default function RootLayout({
                         <div className="w-full  flex min-h-screen relative  flex-col justify-start items-start">
                         {children}
                         </div>
+                        <footer className="flex w-full px-8 md:px-4  justify-center items-center ">
+            <div className="flex w-full  justify-between py-4 text-xs md:text-sm items-center border-t border-neutral-700/30">
+              <Link href="https://leonelyimga.com" className="text-neutral-600"><span className="text-neutral-700">Build By</span> <b>Leonel Yimga</b> </Link>
+              <Link href="https://www.figma.com/design/9kI9yPEyWVhnkW79wpvtHy/Game-Shop-Cat-Game-(Community)?node-id=2-2&t=QZ2SjEMn2a8kHYEu-0" className="text-neutral-600"><span className="text-neutral-700/30">Inspired By</span> <b>This design </b> </Link>
+            </div>
+            </footer>
                     </div>
 
                 
